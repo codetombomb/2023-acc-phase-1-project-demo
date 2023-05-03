d81de9c9ec86cc3f3a109d22f0f0c7d7010b0548
Tom Tobar <cocdetombomb@gmail.com>
2023-05-03 10:26:54 -0500

    add no items found message

--- scripts/renderCards.js
+++ scripts/renderCards.js
@@ -1,5 +1,9 @@
 const renderCards = (cardDataArray) => {
     const cardWrapper = idElement("card-wrapper")
+    if(cardDataArray.length === 0){
+        cardWrapper.innerText = 'No items found'
+        return
+    }
     cardWrapper.innerText = ''
     cardDataArray.forEach(card => {
         const img = document.createElement("img")
